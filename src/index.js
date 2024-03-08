050572d20ace0b4a330d4bb67c6abfe0fdd8f998
DeepakSilaych <deepaksilaych@gmail.com>
2024-03-08 22:53:07 +0530

    go for it

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; 
-import Home from './Pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/mainlayout';
-import SOS from './Pages/sos';
-import KSP from './Pages/ksp';
-import ITSP from './Pages/itsp';
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainLayout>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sos" element={<SOS />} />
-          <Route path="/ksp" element={<KSP />} />
-          <Route path="/itsp" element={<ITSP />} />
-        </Routes>
-      </Router>
-    </MainLayout>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
